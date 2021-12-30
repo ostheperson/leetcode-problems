3bba7595452612326693d797dc218bc2cdee6375
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2021-12-30 21:28:12 +0100

    add: linked-list-cycle

--- easy/Linked-List-Cycle.py
+++ easy/Linked-List-Cycle.py
@@ -1,9 +1,30 @@
 #  Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+class Node:
+    def __init__(self, val=None, nxt=None):
+        self.val = val
+        self.next = nxt
 
-class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        
+class Solution():
+    def hasCycle(self, head: Node) -> bool:
+        """ 
+        initialize slow and fast pointers,
+        loop if non of them are None
+        if slow and fast.next are not the same, 
+            set slow to next node and fast to next two nodes
+        """
+        slow = fast = head
+        while slow and fast:
+            if slow == fast.next:
+                return True
+            slow = slow.next
+            if fast.next: fast = fast.next.next
+        return False
+
+# myList = MyLinkedList()
+# myList.addAtHead(0)
+# myList.addAtIndex(1, 1)
+# myList.addAtIndex(2, 2)
+# myList.addAtTail(3)
+
+# sol = Solution()
+# print (sol.hasCycle(myList.getNode(0)))
