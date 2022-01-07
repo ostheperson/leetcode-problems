cb58637408d18bccae1867dec85ded6a83f6623f
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2022-01-07 10:06:40 +0100

    refactor: intersection of rectangles

--- easy/Intersection-of-Rectangles.py
+++ easy/Intersection-of-Rectangles.py
@@ -4,9 +4,7 @@ rec1 = [0,0,2,2]
 """
 # better memory
 def intersect(rec1, rec2):
-    if rec2[1] >= rec1[3] or rec2[3] <= rec1[1]:
-        return False
-    if rec2[0] >= rec1[2] or rec2[2] <= rec1[0]:
+    if rec2[1] >= rec1[3] or rec2[3] <= rec1[1] or rec2[0] >= rec1[2] or rec2[2] <= rec1[0]:
         return False
     return True
 #  better runtime
