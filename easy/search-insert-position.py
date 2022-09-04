ae4de0e58030faf94aaf0511e90f1e8b0e93b7e3
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2022-09-05 00:54:13 +0100

    add: search insert postion

---
+++ easy/search-insert-position.py
@@ -0,0 +1,18 @@
+def searchInsert(nums, target: int) -> int:
+    """
+    - find first item bigger than target
+    - return index
+    """
+    left = 0
+    right = len(nums) - 1
+    while (left <= right):
+        mid = (left + right) >> 1
+        if nums[mid] == target: return mid
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+    
+
+print (searchInsert([1,4,6,7,9], 3))
