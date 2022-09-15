b81260bc8598fc817f5e43023164bc50b987d359
Ojo Oluwaseyi Shadrach <31080918+OS-the-Person@users.noreply.github.com>
2022-09-15 18:40:40 +0100

    fix: bag of tokens

--- medium/bag-of-tokens.py
+++ medium/bag-of-tokens.py
@@ -1,9 +1,19 @@
 
 def bagOfTokensScore(tokens, power) -> int:
-    score = 0
-    for index in range(len(tokens)):
-        if token[index]
-
+    score, res = 0, 0
+    left, right = 0, len(tokens) - 1
+    while left <= right:
+        if power >= tokens[left]:
+            power -= tokens[left]
+            score += 1
+            res = max(score, res)
+            left += 1
+        elif score > 0:
+            power += tokens[right]
+            score -= 1
+            right -= 1
+        else: break
+    return res
 
 print (bagOfTokensScore([1,4,5,6], 4))
 
